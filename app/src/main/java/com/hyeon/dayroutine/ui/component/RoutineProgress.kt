7e772246aa8877c213f2c61e9f18635dc2d725fd
KJ-Hyeon <bark052526@gmail.com>
2026-04-24 21:11:28 +0900

    refactor: convert progress radius to const val

--- app/src/main/java/com/hyeon/dayroutine/ui/component/RoutineProgress.kt
+++ app/src/main/java/com/hyeon/dayroutine/ui/component/RoutineProgress.kt
@@ -3,18 +3,13 @@ package com.hyeon.dayroutine.ui.component
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.ProgressIndicatorDefaults.drawStopIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
-
+private const val PROGRESS_BAR_RADIUS = 3f
 @Composable
 fun RoutineProgress(
     modifier: Modifier,
@@ -25,16 +20,14 @@ fun RoutineProgress(
             .fillMaxWidth()
             .height(6.dp)
     ) {
-        val radius = size.height / 2
-
         drawRoundRect(
             color = Color(0xFF33334a),
-            cornerRadius = CornerRadius(radius)
+            cornerRadius = CornerRadius(PROGRESS_BAR_RADIUS)
         )
 
         drawRoundRect(
             color = Color(0xFF6c63ff),
-            cornerRadius = CornerRadius(radius),
+            cornerRadius = CornerRadius(PROGRESS_BAR_RADIUS),
             size = Size(width = size.width * progress, height = size.height)
         )
     }

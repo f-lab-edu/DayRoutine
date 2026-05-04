9c50a51562391fbea2d87740179455cb401ee44c
KJ-Hyeon <bark052526@gmail.com>
2026-05-04 11:02:31 +0900

    feat: add HomeViewModel with MVI pattern

---
+++ app/src/main/java/com/hyeon/dayroutine/feature/home/HomeViewModel.kt
@@ -0,0 +1,61 @@
+package com.hyeon.dayroutine.feature.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hyeon.dayroutine.domain.model.Routine
+import com.hyeon.dayroutine.domain.model.RoutineCompletion
+import com.hyeon.dayroutine.domain.repository.RoutineRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import com.hyeon.dayroutine.feature.home.HomeContract.*
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val routineRepository: RoutineRepository
+) : ViewModel() {
+    private val _state = MutableStateFlow(HomeState())
+    val state = _state.asStateFlow()
+
+    fun handleIntent(intent: HomeIntent) {
+        when (intent) {
+            is HomeIntent.CheckRoutine -> checkRoutine(intent.routine, intent.isChecked)
+            is HomeIntent.ClickRoutine -> clickRoutine(intent.routine)
+            is HomeIntent.SelectDate -> selectDate(intent.date)
+            HomeIntent.ShowAddBottomSheet -> showAddBottomSheet()
+            HomeIntent.HideAddBottomSheet -> hideAddBottomSheet()
+        }
+    }
+
+    private fun checkRoutine(routine: Routine, isChecked: Boolean) {
+        val routineCompletion = RoutineCompletion(
+            routineId = routine.id,
+            date = _state.value.selectedDate.dayOfWeek,
+            isComplete = !isChecked
+        )
+
+        viewModelScope.launch {
+            routineRepository.updateCompletion(routineCompletion)
+        }
+    }
+
+    private fun clickRoutine(routine: Routine) {
+        /** Routine Detail Screen 이동 **/
+    }
+
+    private fun selectDate(date: LocalDate) {
+        _state.update { it.copy(selectedDate = date) }
+    }
+
+    private fun showAddBottomSheet() {
+        _state.update { it.copy(isShowAddRoutineBottomSheet = true) }
+    }
+
+    private fun hideAddBottomSheet() {
+        _state.update { it.copy(isShowAddRoutineBottomSheet = true) }
+    }
+}
